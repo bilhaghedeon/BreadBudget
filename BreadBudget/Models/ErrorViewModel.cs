4474b990e5cfa07053128af2f2bb39e62837c57a
Nevada Graci <gracine@sheridancollege.ca>
2019-10-01 09:08:33 -0400

    Add project files.

---
+++ BreadBudget/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BreadBudget.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

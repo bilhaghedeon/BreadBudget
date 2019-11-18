8010bbca0dffc2b2b9e3b3485e2565a82257fbaf
Nevada Graci <gracine@sheridancollege.ca>
2019-11-18 16:32:07 -0500

    Created a UserDb class that will store the user data

---
+++ BreadBudget/Models/UserDb.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BreadBudget.Models
+{
+    public class UserDb : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+
+        public UserDb(DbContextOptions<UserDb> options) : base(options)
+        {
+
+        }
+    }
+}

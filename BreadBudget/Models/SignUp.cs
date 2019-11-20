3e29c3861440e6d28248d4d6f2a91f00b5339adf
Samina <saminak77@gmail.com>
2019-11-20 15:05:27 -0500

    Added Email Validation for Sign Up

--- BreadBudget/Models/SignUp.cs
+++ BreadBudget/Models/SignUp.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BreadBudget.Models
 {
@@ -14,6 +15,7 @@ namespace BreadBudget.Models
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Please enter your email")]
+        
         public string Email { get; set;  }
 
         [Required(ErrorMessage = "Please enter your password")]
